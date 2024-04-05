61a471e53a75bc7c57d0763a7954f4cc2c1cfd54
lht <2660157412@qq.com>
2024-04-05 23:45:04 +0800

    添加了common文件夹，定义了Result类用于统一返回数据类型格式

---
+++ admin/src/main/java/com/jyb/common/Result.java
@@ -0,0 +1,44 @@
+package com.jyb.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Result {
+	private static final String SUCCESS = "0";
+	private static final String ERROR = "-1";
+
+//	调用成功或识别，0成功，-1失败
+	private String code;
+//	如果报错，报错信息为msg
+	private String msg;
+//	返回的信息
+	private Object data;
+//定义返回调用成功状态的方法（不含数据）
+	public static Result seccess(){
+		Result result = new Result();
+		result.setCode(SUCCESS);
+		return result;
+	}
+
+	//定义返回调用成功状态的方法（含数据）
+	public static Result seccess(Object object){
+		Result result = new Result();
+		result.setCode(SUCCESS);
+		result.setData(object);
+		return result;
+	}
+
+//	定义错误方法，返回调用失败状态和报错信息
+	public static Result error(String msg){
+		Result result = new Result();
+		result.setCode(ERROR);
+		result.setMsg(msg);
+		return result;
+	}
+
+
+}
